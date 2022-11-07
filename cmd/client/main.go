3edf1e6a9108e2d1ba6fd5dd6e848932f1f9e4ed
Aditya Saxena <aditya.saxena@goniyo.com>
2022-11-07 12:48:59 +0530

    server side timeout and req cancel check added

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,6 +7,7 @@ import (
 	"grpc-go-poc/pb"
 	"grpc-go-poc/sample"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -29,7 +30,11 @@ func main() {
 		Laptop: laptop,
 	}
 
-	res, err := laptopClient.CreateLaptop(context.Background(), &req)
+	// Timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := laptopClient.CreateLaptop(ctx, &req)
 	if err != nil {
 		log.Fatal("Error in creating laptop request: ", err)
 	}
--- service/laptop_server.go
+++ service/laptop_server.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 	"grpc-go-poc/pb"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // Server that provides laptop services
@@ -18,6 +22,18 @@ func NewLaptopServer() *LaptopServer {
 // CreateLaptop is a Unary RPC to create a new Laptop
 func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
 	// Save laptop to store
+	time.Sleep(6 * time.Second)
+
+	if ctx.Err() == context.Canceled {
+		fmt.Println("request deadline is cancelled")
+		return nil, status.Error(codes.Canceled, "request deadline is cancelled")
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		fmt.Println("request deadline is exceeded")
+		return nil, status.Error(codes.DeadlineExceeded, "request deadline is exceeded")
+	}
+
 	laptop := req.GetLaptop()
 	fmt.Printf("saved laptop with id: %s", laptop.Id)
 
